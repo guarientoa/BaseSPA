7ad02b2e16aa5844ea8e3b6acc3769b2408117fa
CGIL\CarminatiP <paolo.carminati@sintel.net>
2018-06-20 14:35:15 +0200

    Fix

--- BaseSPA.Core/Models/Survey.cs
+++ BaseSPA.Core/Models/Survey.cs
@@ -3,24 +3,20 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BaseSPA.Core.Models
 {
-    using System;
-    using System.Collections.Generic;
+	using System;
+	using System.Collections.Generic;
 	[Table("Survey")]
 
-	public partial class Survey
-    {
-        public Survey()
-        {
-            Questions = new HashSet<Question>();
-        }
+	public class Survey
+	{
 		[Key]
 		public int Id { get; set; }
-        public string Title { get; set; }
-        public string Subtitle { get; set; }
-        public string Description { get; set; }
-        public string Notes { get; set; }
-        public DateTime? dtAgg { get; set; }
+		public string Title { get; set; }
+		public string Subtitle { get; set; }
+		public string Description { get; set; }
+		public string Notes { get; set; }
+		public DateTime? dtAgg { get; set; }
 
-        public virtual ICollection<Question> Questions { get; set; }
-    }
+		public virtual ICollection<Question> Questions { get; set; }
+	}
 }
